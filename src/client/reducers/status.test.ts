57155b4fb64a34eceee5951e640d53d478e05cc5
Jacob Grahn <contact@jacobgrahn.com>
2017-05-22 12:53:58 -0400

    unit tests

--- src/client/reducers/status.test.ts
+++ src/client/reducers/status.test.ts
@@ -1,6 +1,6 @@
 import status from './status'
-import { joinResult } from './room-actions'
-import { JOINED } from './room-states'
+import { joinResult, leave, join } from './room-actions'
+import { JOINED, NOT_JOINING, JOINING } from './room-states'
 
 test('status is set on join success', () => {
   const state = ''
@@ -13,4 +13,24 @@ test('status is set on join success', () => {
   }
   const action = joinResult(roomId, result)
   expect(status(state, action)).toEqual(JOINED)
+})
+
+test('status is set on leave', () => {
+  const state = ''
+  const roomId = ''
+  const action = leave(roomId)
+  expect(status(state, action)).toEqual(NOT_JOINING)
+})
+
+test('status is set on join request', () => {
+  const state = ''
+  const roomId = ''
+  const action = join(roomId)
+  expect(status(state, action)).toEqual(JOINING)
+})
+
+test('status has a default', () => {
+  const state = undefined
+  const action = {type: 'whatev'}
+  expect(status(state, action)).toEqual(NOT_JOINING)
 })
