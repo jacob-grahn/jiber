dbc9652ce09500b397fb747cd75a5769c9a007db
Jacob Grahn <contact@jacobgrahn.com>
2017-07-06 13:04:15 -0400

    add an example app

---
+++ examples/draw-with-friends/server/README.md
@@ -0,0 +1,5 @@
+### install dependencies
+npm install
+
+### start the server
+npm start
---
+++ examples/draw-with-friends/server/index.js
@@ -0,0 +1,3 @@
+const hope = require('hope-server')
+hope.createStore()
+console.log('Draw with friends is running!')
