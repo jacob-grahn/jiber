e4171024791edb15815b634d76af3583ecec9bb9
Jacob Grahn <contact@jacobgrahn.com>
2017-07-11 15:44:21 -0400

    tests

---
+++ src/core/utils/get.test.ts
@@ -0,0 +1,24 @@
+import get from './get'
+
+test('non object or array input values result in undefined', () => {
+  expect(get(undefined, 'something')).toBe(undefined)
+  expect(get(null, 'something')).toBe(undefined)
+  expect(get(true, 'something')).toBe(undefined)
+  expect(get(false, 'something')).toBe(undefined)
+  expect(get('', 'something')).toBe(undefined)
+})
+
+test('deep paths work', () => {
+  const user = {name: {first: 'sue', last: 'per', age: null}}
+  expect(get(user, 'name.first')).toBe('sue')
+  expect(get(user, 'name.age')).toBe(null)
+})
+
+test('bad paths don\'t throw an error, they just return undefined', () => {
+  expect(get({}, 'name.first.wee')).toBe(undefined)
+})
+
+test('arrays should work', () => {
+  const users = [{name: 'bob'}, {name: 'sue'}]
+  expect(get(users, '0.name')).toBe('bob')
+})
--- src/core/utils/get.ts
+++ src/core/utils/get.ts
@@ -1,7 +1,10 @@
 export default function get (value: any, strPath: string): any {
   const paths = strPath.split('.')
   return paths.reduce(
-    (value, path) => value ? value[path] : value,
+    (value, path) => {
+      if (!value) return undefined
+      return value ? value[path] : value
+    },
     value
   )
 }
