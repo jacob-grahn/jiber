4d949d08c33009ca0de1c7b3cee7e29e09e9906e
Jacob Grahn <contact@jacobgrahn.com>
2017-07-11 15:31:32 -0400

    mongo backed storage engine

---
+++ src/storage-mongo/index.js
@@ -0,0 +1,104 @@
+const MongoClient = require('mongodb').MongoClient
+const settings = {
+  mongoUrl: 'mongodb://localhost:27017/hopetest',
+  mongoActionCollection: 'actions',
+  mongoRoomCollection: 'rooms'
+}
+const db = toPromise(MongoClient, 'connect', settings.mongoUrl)
+
+async function getDb () {
+  return db
+}
+
+function toPromise () {
+  const [caller, func, ...args] = Array.from(arguments)
+  return new Promise((resolve, reject) => {
+    caller[func](...args, (err, result) => {
+      if (err) reject(err)
+      resolve(result)
+    })
+  })
+}
+
+async function pushAction (roomId, action) {
+  const db = await getDb()
+  const collection = db.collection(settings.mongoActionCollection)
+  action._hopeRoomId = action.$hope.roomId
+  action._hopeUserId = action.$hope.userId
+  delete action.$hope
+
+  // strange update to get around the fact that $currentDate doesn't work with inserts
+  // https://stackoverflow.com/questions/20620368/is-there-any-equivalent-of-now-in-mongodb/37061284#37061284
+  await toPromise(
+    collection,
+    'update',
+    {_id: {$lt: 0}},
+    {$set: action, $currentDate: {'_hopeDate': true}},
+    {upsert: true}
+  )
+}
+
+async function fetchActions (roomId, minTimeMs) {
+  const db = await getDb()
+  const collection = db.collection(settings.mongoActionCollection)
+  const actionCursor = await toPromise(
+    collection,
+    'find',
+    {_hopeDate: {$gt: new Date(minTimeMs)}}
+  )
+  const actions = await actionCursor.toArray()
+  const parsedActions = actions.map(action => {
+    action.$hope = {
+      userId: action._hopeUserId,
+      roomId: action._hopeRoomId,
+      timeMs: new Date(action._hopeDate).getTime()
+    }
+    delete action._hopeUserId
+    delete action._hopeRoomId
+    delete action._hopeDate
+    return action
+  })
+  return parsedActions
+}
+
+async function removeActions (roomId, minTimeMs) {
+  const db = await getDb()
+  const collection = db.collection(settings.mongoActionCollection)
+  await toPromise(
+    collection,
+    'removeMany',
+    {_hopeDate: {$lte: new Date(minTimeMs)}}
+  )
+}
+
+async function fetchState (roomId) {
+  const db = await getDb()
+  const collection = db.collection(settings.mongoRoomCollection)
+  const state = await toPromise(collection, 'findOne', {_id: roomId})
+  if (!state) {
+    return {
+      members: {},
+      confirmedState: undefined,
+      lastUpdatedAt: 0
+    }
+  }
+  return state
+}
+
+async function storeState (roomId, state) {
+  const db = await getDb()
+  const collection = db.collection(settings.mongoRoomCollection)
+  await toPromise(
+    collection,
+    'update',
+    {_id: roomId}, {$set: state}, {upsert: true}
+  )
+}
+
+module.exports = {
+  pushAction,
+  fetchActions,
+  removeActions,
+  fetchState,
+  storeState
+}
