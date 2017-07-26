11cd53b1bfb97097fa5f46b19aeaa1bed1b85c34
Jacob Grahn <contact@jacobgrahn.com>
2017-07-26 11:19:55 -0400

    tests

--- src/client/create-client-store.test.ts
+++ src/client/create-client-store.test.ts
@@ -1,3 +1,9 @@
-test('todo', () => {
-  expect(false).toBe(true)
+import createClientStore from './create-client-store'
+
+test('it should make a working client store', () => {
+  const clientStore = createClientStore()
+  expect(clientStore).toBeTruthy()
+  expect(clientStore.getState()).toBeTruthy()
+  expect(clientStore.createRoom('room1')).toBeTruthy()
+  expect(clientStore.dispatch({type: 'test'})).toBeUndefined()
 })
