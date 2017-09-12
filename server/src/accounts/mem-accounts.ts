d1f378f37ee484d6cc9b8ba02d12f871f47bcf30
Jacob Grahn <contact@jacobgrahn.com>
2017-09-12 15:06:38 -0400

    todo is done

--- server/src/accounts/mem-accounts.ts
+++ server/src/accounts/mem-accounts.ts
@@ -3,7 +3,6 @@ import { randStr } from '../utils/rand-str'
 
 /**
  * Dummy account system that gives every login request a new accountId
- * todo: split out these other functions
  */
 export const memAccounts = async (): Promise<User> => {
   return {
