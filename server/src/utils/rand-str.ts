8d7d270709583e01ba22017b8d3e756663768ed8
Jacob Grahn <contact@jacobgrahn.com>
2017-09-13 14:41:28 -0400

    tests

--- server/src/utils/rand-str.ts
+++ server/src/utils/rand-str.ts
@@ -1,7 +1,7 @@
 import * as crypto from 'crypto'
 
 export const randStr = (len: number) => {
-  const buf = crypto.randomBytes(256)
+  const buf = crypto.randomBytes(Math.ceil(len / 2))
   const str = buf.toString('hex')
-  return str.replace(/[W]/g, '').substr(0, len)
+  return str.substr(0, len)
 }
