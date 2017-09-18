80aa7fb008b8da61e8fe18d1ee8199e35dc0ef9f
Jacob Grahn <contact@jacobgrahn.com>
2017-09-18 17:22:30 -0400

    add an error handler if the ws server can't start

--- examples/draw-with-friends/server/index.js
+++ examples/draw-with-friends/server/index.js
@@ -1,9 +1,6 @@
 const jiber = require('jiber-server')
 const dbRedis = require('jiber-db-redis')
 
-console.log({dbRedis})
-console.log(dbRedis.createDb)
-
 const db = dbRedis.createDb({host: '127.0.0.1', port: 6379})
 const store = jiber.createStore({db})
 
