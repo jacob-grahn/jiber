90633cf0fa058061c70b0f948a91af8c43f1073c
Jacob Grahn <contact@jacobgrahn.com>
2017-10-02 16:11:14 -0400

    gaining a vauge sense of how this will work

--- packages/jiber-client/src/webrtc/connection.ts
+++ packages/jiber-client/src/webrtc/connection.ts
@@ -5,15 +5,45 @@
 // todo: when to close connections?
 // todo: set a maximum number of connections
 // todo: I have no idea what I am doing
+// https://shanetully.com/2014/09/a-dead-simple-webrtc-example/
+// https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Signaling_and_video_calling
 
-const pcConfig: RTCConfiguration = {iceServers: []}
+// standardize browser prefixes
+window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
+window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
+
+// todo: remove these public stun servers and supply them via config
+const pcConfig: RTCConfiguration = {
+  iceServers: [
+    {urls: 'stun:stun3.l.google.com:19302'},
+    {urls: 'stun:stunserver.org'}
+  ]
+}
 const channelConfig: any = {
   ordered: false,
   negotiated: true,
   maxRetransmits: 0
 }
+
 const pc = new RTCPeerConnection(pcConfig)
 const channel = pc.createDataChannel('testChannel', channelConfig)
+
+pc.createOffer()
+.then((offer: RTCSessionDescriptionInit) => {
+  pc.setLocalDescription(offer)
+  sendOffer(offer)
+})
+
+const receiveIceCandidate = (candidate) => {
+  pc.addIceCandidate(new RTCIceCandidate(candidate))
+}
+
+pc.onicecandidate = (candidate) => {
+  console.log('onicecandidate', candidate)
+  if (!candidate) return
+  sendIceCandidate(candidate)
+}
 
 channel.onerror = (error: any) => {
   console.log("channel.onerror", error)
