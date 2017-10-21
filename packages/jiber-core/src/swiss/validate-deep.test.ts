a2d721acc898f2997bde3a75e719c1b53f261bf0
Jacob Grahn <contact@jacobgrahn.com>
2017-10-20 22:16:59 -0400

    tests

--- packages/jiber-core/src/swiss/validate-deep.test.ts
+++ packages/jiber-core/src/swiss/validate-deep.test.ts
@@ -1,1 +1,24 @@
-import { validateDeep } from 'validate-deep'
+import { validateDeep } from './validate-deep'
+
+test('strings and numbers should validate normally', () => {
+  const rules = {
+    name: {'.type': 'string'}
+  }
+  expect(validateDeep(rules, 'name', 'bob')).toBe(true)
+  expect(validateDeep(rules, 'name', 5)).toBe(false)
+})
+
+test('objects and all of their child data is checked', () => {
+  const rules = {
+    people: [{
+      name: {
+        first: {'.type': 'string'},
+        last: {'.type': 'string'}
+      }
+    }]
+  }
+  const goodName = {first: 'Billy', last: 'Bob'}
+  const badName = {firest: 'Billy', middle: 'Bob', last: 'Thorton'}
+  expect(validateDeep(rules, 'people.7.name', goodName)).toBe(true)
+  expect(validateDeep(rules, 'people.7.name', badName)).toBe(false)
+})
--- packages/jiber-core/src/swiss/validate-deep.ts
+++ packages/jiber-core/src/swiss/validate-deep.ts
@@ -4,7 +4,7 @@ import { forEach } from '../utils/for-each'
 
 export const validateDeep = (rules: any, path: string, value: any): boolean => {
   if (typeof value === 'object') {
-    const flat = flatten(value, path)
+    const flat = flatten(value, `${path}.`)
     const badPaths: string[] = []
     forEach(flat, (subValue: string|number, subPath: string) => {
       if (!validate(rules, subPath, subValue))
