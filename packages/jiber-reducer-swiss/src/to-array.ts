a7f572fd882ce53cf08321e37db7a7bcda2a498f
Jacob Grahn <contact@jacobgrahn.com>
2018-01-19 19:04:47 -0500

    That was getting really complex

--- packages/jiber-reducer-swiss/src/to-array.ts
+++
@@ -1,8 +0,0 @@
-/**
- * coerse a value into an array
- */
-export const toArray = (value: any): any[] => {
-  if (Array.isArray(value)) return value
-  if (value === undefined) return []
-  return [value]
-}
--- packages/jiber-reducer-swiss/src/validate/to-zeroes.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Convert numbers in a path to 0
- * Example: ['assets', '55', 'name'] becomes ['assets', '0', 'name']
- * While this may seem like an odd thing to want to do, it is
- * handy for looking values up in a validation object
- */
-export const toZeroes = (arr: string[]): string[] => {
-  return arr.map(key => /^[0-9].*$/.test(key) ? '0' : key)
-}
