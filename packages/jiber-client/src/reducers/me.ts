211b07b6244a70e069264f54914e1f4aa08c3222
Jacob Grahn <contact@jacobgrahn.com>
2018-01-23 13:34:10 -0500

    update

--- packages/jiber-client/src/reducers/me.ts
+++ packages/jiber-client/src/reducers/me.ts
@@ -4,7 +4,7 @@ import { Action, User, LOGIN_RESULT } from 'jiber-core'
  * Keep track of the currently logged in user
  * @hidden
  */
-export const me = (state: User, action: Action): User => {
+export const me = (state: User | undefined, action: Action): User | undefined => {
   switch (action.type) {
     case LOGIN_RESULT:
       return action.user
