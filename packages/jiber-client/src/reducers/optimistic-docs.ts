1653d3dc5eb2abe9cf202ed1e6c340c4b553dc17
Jacob Grahn <contact@jacobgrahn.com>
2018-01-26 06:31:44 -0500

    wip

--- packages/jiber-client/src/reducers/optimistic-docs.ts
+++ packages/jiber-client/src/reducers/optimistic-docs.ts
@@ -8,16 +8,20 @@ import { Reducer, SERVER, dictionary } from 'jiber-core'
  */
 
 const optimisticDoc = (reducer: Reducer) => (state: any, action: any) => {
+  // hack to clean up the other hack to get this info into this reducer
+  const docs = action.$$docs
+  const optimisticActions = action.$$optimisticActions
+  delete action.$$docs
+  delete action.$$optimisticActions
+
   if (action.$src === SERVER) {
-    if (!action.$$docState || !action.$$optimisticActions) return state
-    const docState = action.$$docState[action.$doc]
-    const optimisticActions = action.$$optimisticActions
+    if (!docs || !optimisticActions) return state
+    const docState = docs[action.$doc]
 
     // copy is not needed if reducer does not mutate state
     // this could possibly be optional via the settings
     // it is just a pain to always write reducers that do not mutate
     const docStateCopy = docState ? JSON.parse(JSON.stringify(docState)) : docState
-
     return optimisticActions.reduce(reducer, docStateCopy)
   }
   return reducer(state, action)
--- packages/jiber-client/src/store/client-reducer.test.ts
+++ packages/jiber-client/src/store/client-reducer.test.ts
@@ -1,4 +1,4 @@
-import { Action } from 'jiber-core'
+import { Action, SERVER, STATE } from 'jiber-core'
 import { createClientReducer } from './client-reducer'
 
 const adder = (state: any = '', action: Action): any => {
@@ -6,8 +6,41 @@ const adder = (state: any = '', action: Action): any => {
 }
 const clientReducer = createClientReducer(adder)
 
-test('it should default to something', () => {
+test('add an optimistic action', () => {
   const state = undefined
-  const action = { type: 'test', $doc: 'mydoc' }
-  expect(clientReducer(state, action)).toBeTruthy()
+  const action = { type: 'test', $doc: 'mydoc', value: '222' }
+  const newState = clientReducer(state, action)
+  expect(newState.optimisticDocs.mydoc).toBe('222')
+  expect(newState.optimisticActions[0]).toBe(action)
+})
+
+test('add a confirmed action', () => {
+  const state = {
+    docs: {
+      mydoc: '123'
+    },
+    optimisticDocs: {
+      mydoc: '123456'
+    },
+    optimisticActions: [
+      {$doc: 'mydoc', value: '4', $madeAt: 4, $uid: 'paul'},
+      {$doc: 'mydoc', value: '5', $madeAt: 5, $uid: 'paul'},
+      {$doc: 'mydoc', value: '6', $madeAt: 6, $uid: 'paul'}
+    ]
+  }
+  const action = { type: 'test', $doc: 'mydoc', value: '4', $madeAt: 4, $src: SERVER, $uid: 'paul' }
+  const newState = clientReducer(state, action)
+  expect(newState.docs.mydoc).toBe('1234')
+  expect(newState.optimisticDocs.mydoc).toBe('123456')
+  expect(newState.optimisticActions).toEqual([
+    {$doc: 'mydoc', value: '5', $madeAt: 5, $uid: 'paul'},
+    {$doc: 'mydoc', value: '6', $madeAt: 6, $uid: 'paul'}
+  ])
+})
+
+test('add a STATE confirmed action', () => {
+  const state = undefined
+  const action = {type: STATE, state: {'31,27': '#BD6919'}, $doc: 'draw', $src: SERVER}
+  const newState = clientReducer(state, action)
+  expect(newState.optimisticDocs.draw).toEqual({'31,27': '#BD6919'})
 })
