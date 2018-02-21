27ac1dec70452edbd8640c29f7ef80b8525d32c3
Jacob Grahn <contact@jacobgrahn.com>
2018-02-20 22:28:35 -0500

    edge cases

--- packages/jiber-client/src/peers/peer-manager.ts
+++ packages/jiber-client/src/peers/peer-manager.ts
@@ -1,24 +1,48 @@
-import { Action, Store, OPEN, CLOSE } from 'jiber-core'
+import { Action, Store, OPEN, CLOSE, WEBRTC_OFFER } from 'jiber-core'
 import { Peer } from './peer'
+
+export interface PeerSettings {
+  store: Store,
+  stunServers: string[],
+  maxPeers: number
+}
 
 export class PeerManager {
   private dict: {[peerId: string]: Peer} = {}
+  private settings: PeerSettings
 
-  constructor (store: Store, stunServers: string[], maxPeers: number) {
-    store.subscribe((_state: any, action: Action) => {
-      const peerId = action.$uid
-      if (!peerId || this.dict[peerId]) return
+  constructor (settings: PeerSettings) {
+    this.settings = settings
+    settings.store.subscribe(this.onLocalAction)
+  }
+
+  private onLocalAction = (_state: any, action: Action) => {
+    const { store, stunServers, maxPeers } = this.settings
+    const peerId = action.$uid
+    if (!peerId) return
+
+    // invite new people to become peers
+    if (action.type === OPEN || action.type === WEBRTC_OFFER) {
+
+      // close unused peer connections to maybe make room for a new one
+      this.prunePeers()
+
+      // don't make the same peer connection twice
+      if (this.dict[peerId]) return
+
+      // don't exceed the maximum number of allowed peers
       if (Object.keys(this.dict).length >= maxPeers) return
 
-      if (action.type === OPEN) {
-        const peer = new Peer({ peerId, store, stunServers })
-        this.dict[peerId] = peer
-      }
+      // create a new peer connection
+      const { offer } = action
+      const peer = new Peer({ peerId, store, stunServers, offer })
+      this.dict[peerId] = peer
+    }
 
-      if (action.type === CLOSE) {
-        this.removePeer(peerId)
-      }
-    })
+    // remove disconnected peers
+    if (action.type === CLOSE) {
+      this.removePeer(peerId)
+    }
   }
 
   private removePeer = (peerId: string) => {
