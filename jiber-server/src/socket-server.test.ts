13503081e57044f295dd64c6f0c5aef77e45d270
Jacob Grahn <contact@jacobgrahn.com>
2019-01-11 21:32:07 -0500

    ssl test

--- jiber-server/src/socket-server.test.ts
+++ jiber-server/src/socket-server.test.ts
@@ -1,15 +1,17 @@
-/* global test, expect */
+/* global test, expect, afterEach */
 
 import { SocketServer } from './socket-server'
-const WebSocket = require('ws')
+import * as WebSocket from 'ws'
+import * as https from 'https'
+import * as fs from 'fs'
 
 test('Listen on a given port', () => {
   return new Promise((resolve) => {
-    const server = new SocketServer(8080)
-    const client = new WebSocket('ws://localhost:8080')
+    const server = new SocketServer({ port: 8080 })
+    const client = new WebSocket.default('ws://localhost:8080')
 
-    client.on('open', function open() {
-      expect(true).toBeTruthy()
+    client.on('open', () => {
+      expect(client.readyState).toEqual(WebSocket.OPEN)
       server.close()
       client.close()
       resolve()
@@ -17,7 +19,35 @@ test('Listen on a given port', () => {
   })
 })
 
-test('Use a custom server', () => {
+test('Use a custom server with ssl', () => {
+  return new Promise((resolve, reject) => {
+    const webServer = https.createServer({
+      cert: fs.readFileSync('./src/server.test.cert'),
+      key: fs.readFileSync('./src/server.test.key')
+    })
+    const server = new SocketServer({ server: webServer })
+    webServer.listen(8081)
+
+    const client = new WebSocket.default(
+      'wss://localhost:8081',
+      { rejectUnauthorized: false }
+    )
+
+    client.on('error', (err: any) => {
+      webServer.close()
+      server.close()
+      client.close()
+      reject(err)
+    })
+
+    client.on('open', () => {
+      expect(client.readyState).toEqual(WebSocket.OPEN)
+      webServer.close()
+      server.close()
+      client.close()
+      resolve()
+    })
+  })
 })
 
 test('Authenticate connection attempts', () => {
--- jiber-server/src/socket-server.ts
+++ jiber-server/src/socket-server.ts
@@ -4,20 +4,32 @@ import { LOGIN_RESULT } from './constants'
 import { Packet } from './packet'
 import { verifyClient as defaultVerifyClient } from './verify-client'
 
+export interface SocketServerOptions {
+  port?: number,
+  verifyClient?: WS.VerifyClientCallbackAsync,
+  server?: any
+}
+
+const defaultOptions: SocketServerOptions = {
+  port: 80,
+  verifyClient: defaultVerifyClient,
+  server: undefined
+}
+
 export class SocketServer {
-  private socketLookup: {[userId: string]: WS} = {}
+  private socketLookup: { [userId: string]: WS } = {}
   private wss: WS.Server
 
-  constructor (
-    port: number = 80,
-    verifyClient?: WS.VerifyClientCallbackAsync,
-    customServer?: any
-  ) {
-    this.wss = new WS.Server({
-      port: customServer ? undefined : port,
-      verifyClient: verifyClient || defaultVerifyClient,
-      server: customServer
-    })
+  constructor (inputOptions: SocketServerOptions) {
+    const options = Object.assign(defaultOptions, inputOptions)
+
+    // if port is defined, it creates and uses an internal server
+    // this is not good if we want to pass in a custom server
+    if (options.server) {
+      delete options.port
+    }
+
+    this.wss = new WS.Server(options)
     this.wss.on('error', logger.error)
     this.wss.on('connection', this.onConnection)
   }
