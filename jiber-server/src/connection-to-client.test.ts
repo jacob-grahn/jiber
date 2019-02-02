50a306b9babb329909f2ec96992714066bc6c3b8
Jacob Grahn <contact@jacobgrahn.com>
2019-02-01 22:37:37 -0500

    lint

--- jiber-server/src/connection-to-client.test.ts
+++ jiber-server/src/connection-to-client.test.ts
@@ -2,8 +2,9 @@
 
 import * as WS from 'ws'
 import { ConnectionToClient } from './connection-to-client'
+import { Packet } from './packet'
 
-test('send a welcome packet when a connection is established', async () => {
+test('send a welcome packet when a connection is established', () => {
   const sends: string[] = []
   const socket: any = {
     readyState: WS.OPEN,
@@ -18,4 +19,27 @@ test('send a welcome packet when a connection is established', async () => {
   expect(sends.length).toBe(1)
   expect(welcomePacket.type).toBe('WELCOME')
   expect(welcomePacket.user).toEqual({ name: 'sue' })
+})
+
+test('emit packets from client', () => {
+  const received: Packet[] = []
+  let handler: Function = () => { /* do nothing */ }
+  const socket: any = {
+    readyState: WS.OPEN,
+    on: (_eventType: string, func: Function) => { handler = func },
+    send: (_message: string) => { /* do nothing */ }
+  }
+
+  const conn = new ConnectionToClient(socket, { id: 'abc' })
+  conn.on('packetFromClient', (packet: Packet) => { received.push(packet) })
+
+  const packet = { payload: 'test' }
+  const message = JSON.stringify(packet)
+  handler(message)
+
+  expect(received.length).toBe(1)
+
+  const receivedPacket = received[0]
+  expect(receivedPacket.user).toEqual({ id: 'abc' })
+  expect(receivedPacket.payload).toBe('test')
 })
